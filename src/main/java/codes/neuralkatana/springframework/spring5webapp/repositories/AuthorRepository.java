ffbc883c1796fa6ecd5bbb3e152399cf6928b7e9
neuralrunner <brunohcagnin@gmail.com>
2020-09-09 13:31:26 -0300

    Adding Spring Data Repositories

---
+++ src/main/java/codes/neuralkatana/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package codes.neuralkatana.springframework.spring5webapp.repositories;
+
+import codes.neuralkatana.springframework.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
---
+++ src/main/java/codes/neuralkatana/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package codes.neuralkatana.springframework.spring5webapp.repositories;
+
+import codes.neuralkatana.springframework.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book,Long> {
+}
